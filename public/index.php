f78206aa87c5ad92fdc05093a1f69a2dd7a5825c
Erik Fonselius <fonsan@gmail.com>
2011-03-22 14:27:14 +0100

    Fixed relative paths

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
-define('BASE_PATH', realpath('../../../') . DIRECTORY_SEPARATOR);
-define('APP_PATH', BASE_PATH . 'demos/blog/app' . DIRECTORY_SEPARATOR);
+define('BASE_PATH', realpath('../') . DIRECTORY_SEPARATOR);
+define('APP_PATH', BASE_PATH . 'app' . DIRECTORY_SEPARATOR);
 define('EXTRAS_PATH', BASE_PATH . 'extras' . DIRECTORY_SEPARATOR);
 define('SYSTEM_PATH', BASE_PATH . 'system' . DIRECTORY_SEPARATOR);
 
@@ -30,7 +30,7 @@ $tree->addParent('CatchAll', '*', '/', 'blog#index');
 $router = new RouterTree('/glenn/demos/blog/public');
 $router->addRoutes($tree->toArray());
 
-require_once BASE_PATH . 'vendor/ActiveRecord/ActiveRecord.php';
+require_once APP_PATH . 'vendor/ActiveRecord/ActiveRecord.php';
 ActiveRecord\Config::initialize(function($cfg) {
 	$cfg->set_model_directory(APP_PATH . 'classes/model');
 	$cfg->set_connections(array('development' => 'sqlite://blog.db'));
